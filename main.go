77a6b327d235bf483d738ce79508a3ae442cae2c
Suhasbt <suhasbt04@gmail.com>
2026-09-25 12:06:23 +0530

    CICD Changes2done12

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 }
 
 func coursePage(w http.ResponseWriter, r *http.Request) {
-	// Render the course html page 1
+	// Render the course html page 12
 	http.ServeFile(w, r, "static/courses.html")
 }
 
